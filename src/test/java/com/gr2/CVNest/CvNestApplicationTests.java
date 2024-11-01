5d79d4bae6295bf6196171c0aaa1c5e4e9b13df8
trankien <kien8b2003@gmail.com>
2024-11-01 21:32:14 +0700

    initial commit

---
+++ src/test/java/com/gr2/CVNest/CvNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr2.CVNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
